46607b443683ae0f8df3417d94180f2c3dd8110e
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-14 12:50:52 -0600

    Update naive_implementation.py

--- parallel_sparse_reward_propagation/code/naive_implementation.py
+++ parallel_sparse_reward_propagation/code/naive_implementation.py
@@ -2,13 +2,12 @@ import torch
 
 def sparse_reward_propagation_naive(rewards, discount=0.99):
     """
-    Naive sequential accumulation:
-    out[:, t] += discount * out[:, t+1]
+    Naive approach that does backward accumulation:
+      for t in reversed(range(S-1)):
+         out[:, t] += discount * out[:, t+1]
     """
-    B, S = rewards.shape
     out = rewards.clone()
-
+    B, S = out.shape
     for t in reversed(range(S - 1)):
         out[:, t] += discount * out[:, t + 1]
-
     return out
