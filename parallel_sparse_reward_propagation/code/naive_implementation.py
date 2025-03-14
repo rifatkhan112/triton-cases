8fd7f2d92193220369ac5f6d2430291e52236053
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-14 16:47:38 -0600

    Update naive_implementation.py

--- parallel_sparse_reward_propagation/code/naive_implementation.py
+++ parallel_sparse_reward_propagation/code/naive_implementation.py
@@ -1,8 +1,16 @@
 import torch
 
-def sparse_reward_propagation_naive(rewards, discount=0.99):
+def sparse_reward_propagation_naive_enhanced(rewards, reward_mask=None, discount=0.99):
     B, S = rewards.shape
     out = rewards.clone()
+    
+    # If no mask provided, assume all rewards are sparse (original behavior)
+    if reward_mask is None:
+        reward_mask = torch.ones_like(rewards, dtype=torch.bool)
+    
     for t in reversed(range(S - 1)):
-        out[:, t] += discount * out[:, t + 1]
+        # Only propagate non-sparse rewards (mask=False)
+        propagate = ~reward_mask[:, t]
+        out[propagate, t] = discount * out[propagate, t + 1]
+        
     return out
