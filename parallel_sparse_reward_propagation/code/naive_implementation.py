247e6367265a47dd81b3f8adb4cf65381441a8fd
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-14 12:33:08 -0600

    Update naive_implementation.py

--- parallel_sparse_reward_propagation/code/naive_implementation.py
+++ parallel_sparse_reward_propagation/code/naive_implementation.py
@@ -1,14 +1,13 @@
 import torch
 
-def sparse_reward_propagation_naive(rewards, sparse_indices, discount=0.99):
+def sparse_reward_propagation_naive(rewards, discount=0.99):
+    """
     B, S = rewards.shape
-    _, K = sparse_indices.shape
+    For t in reversed(range(S-1)):
+        out[:, t] += discount * out[:, t+1]
+    """
     out = rewards.clone()
-
-    for b in range(B):
-        for k in range(K):
-            idx = sparse_indices[b, k]
-            if idx >= 0 and idx < S - 1:
-                out[b, idx] += discount * out[b, idx + 1]
-    
+    B, S = out.shape
+    for t in reversed(range(S - 1)):
+        out[:, t] += discount * out[:, t + 1]
     return out
