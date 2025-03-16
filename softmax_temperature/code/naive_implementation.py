653a1f586dd413a06373fe8de032d5fcc05fb5f2
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-16 13:07:35 -0600

    Update and rename naive_implementation to naive_implementation.py

---
+++ softmax_temperature/code/naive_implementation.py
@@ -0,0 +1,19 @@
+import torch
+
+def naive_softmax(x, t):
+    """Compute row-wise softmax of X using native pytorch
+
+    We subtract the maximum element to avoid overflows. Softmax is invariant to this shift.
+    """
+    # read  MN elements ; write M  elements
+    x_max = x.max(dim=1)[0] / t
+    # read MN + M elements ; write MN elements
+    z = x / t - x_max[:, None]
+    # read  MN elements ; write MN elements
+    numerator = torch.exp(z)
+    # read  MN elements ; write M  elements
+    denominator = numerator.sum(dim=1)
+    # read MN + M elements ; write MN elements
+    ret = numerator / denominator[:, None]
+    # in total: read 5MN + 2M elements ; wrote 3MN + 2M elements
+    return ret
