edb4d97ec0eead7496f57dfccf4128e91743fefd
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-17 14:04:28 -0600

    Update naive_implementation.py

--- softmax_temperature/code/naive_implementation.py
+++ softmax_temperature/code/naive_implementation.py
@@ -1,19 +1,31 @@
 import torch
+import triton
+import triton.language as tl
+DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}')
 
-def naive_softmax(x, tau):
-    """Compute row-wise softmax of X using native pytorch
-
-    We subtract the maximum element to avoid overflows. Softmax is invariant to this shift.
-    """
-    # read  MN elements ; write M  elements
-    x_max = x.max(dim=1)[0]
-    # read MN + M elements ; write MN elements
-    z = x / tau - x_max[:, None]
-    # read  MN elements ; write MN elements
-    numerator = torch.exp(z / tau)
-    # read  MN elements ; write M  elements
+######### Step 1 #########
+# first we'll look at the naive implementation jic you need a refresher
+def naive_softmax(x):
+    '''
+    Built for input of size (M,N)
+    Safe softmax is when we subtract the maximum element in order to avoid numerical 
+    overflows when doing .exp(); softmax is invariant to this shift
+    '''
+    # read MN elements, find their max along N, and write M elements (the maxes)
+    x_max = x.max(dim=1)[0] 
+        # pytorch actually outputs a tuple of (values, indices) so [0] grabs the values;
+        # we ignored the indices when talking about memory writes above
+    # read MN + M elements, subtraction is MN flops, and write MN elements
+    tau = 100
+    z = (x - x_max[:, None]) / tau
+    # read MN elements and write MN elemnts
+    numerator = torch.exp(z)
+        # exp is actually a lot of flops per element but we're only worried about mem ops rn
+    # read MN elements, do MN flops to find M sum values, and then write M elements
     denominator = numerator.sum(dim=1)
-    # read MN + M elements ; write MN elements
-    ret = numerator / denominator[:, None]
-    # in total: read 5MN + 2M elements ; wrote 3MN + 2M elements
-    return ret
+    # read MN + M elements, division is MN flops, then write MN elements
+    out = numerator / denominator[:, None]
+
+    # in total we did 8MN + 4M memory operations
+    # (read 5MN + 2M elements; wrote 3MN + 2M elements)
+    return out
