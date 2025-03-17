b2218340137243c097d07edbe6046fd77db5358c
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-17 12:11:44 -0600

    Create test.py

---
+++ softmax_temperature/tests/test.py
@@ -0,0 +1,7 @@
+
+
+torch.manual_seed(0)
+x = torch.randn(1823, 781, device=DEVICE)
+y_triton = softmax(x)
+y_torch = torch.softmax(x, axis=1)
+assert torch.allclose(y_triton, y_torch), (y_triton, y_torch)
