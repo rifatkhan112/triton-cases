53c8fcbd1ff40f89ac18f9f35c5cf5e48b19f04a
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-17 13:25:37 -0600

    Update triton_implementation.py

--- softmax_temperature/code/triton_implementation.py
+++ softmax_temperature/code/triton_implementation.py
@@ -1,53 +1,97 @@
 import torch
 import triton
 import triton.language as tl
-from typing import Any, Optional, Union
+from triton.runtime import driver
+
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
+
+def is_hip():
+    return triton.runtime.driver.active.get_current_target().backend == "hip"
+
+def is_cdna():
+    return is_hip() and triton.runtime.driver.active.get_current_target().arch in ('gfx940', 'gfx941', 'gfx942', 'gfx90a', 'gfx908')
 
 @triton.jit
-def softmax_activation_kernel(
-    x_ptr, output_ptr, axis_ld, n_elements, BLOCK_SIZE: tl.constexpr, tau
-):
-    """
-    Softmax activation function kernel
+def softmax_kernel(output_ptr, input_ptr, input_row_stride, output_row_stride, n_rows, n_cols, BLOCK_SIZE: tl.constexpr, num_stages: tl.constexpr, tau: tl.constexpr):
+    # starting row of the program
+    row_start = tl.program_id(0)
+    row_step = tl.num_programs(0)
+    for row_idx in tl.range(row_start, n_rows, row_step, num_stages=num_stages):
+        # The stride represents how much we need to increase the pointer to advance 1 row
+        row_start_ptr = input_ptr + row_idx * input_row_stride
+        # The block size is the next power of two greater than n_cols, so we can fit each
+        # row in a single block
+        col_offsets = tl.arange(0, BLOCK_SIZE)
+        input_ptrs = row_start_ptr + col_offsets
+        # Load the row into SRAM, using a mask since BLOCK_SIZE may be > than n_cols
+        mask = col_offsets < n_cols
+        row = tl.load(input_ptrs, mask=mask, other=-float('inf'))
+        # Subtract maximum for numerical stability
+        row_minus_max = row - tl.max(row, axis=0)
+        # Note that exponentiation in Triton is fast but approximate (i.e., think __expf in CUDA)
+        numerator = tl.exp(row_minus_max / tau)
+        denominator = tl.sum(numerator, axis=0)
+        softmax_output = numerator / denominator
+        # Write back output to DRAM
+        output_row_start_ptr = output_ptr + row_idx * output_row_stride
+        output_ptrs = output_row_start_ptr + col_offsets
+        tl.store(output_ptrs, softmax_output, mask=mask)
 
-    Computes the function which rescales elements to the range`[0, 1]`: {softmax}(x) = \frac{\exp(x_i)}{\sum_j \exp(x_j)}
-    """
-    pid = tl.program_id(axis=0)
-    block_start = pid * BLOCK_SIZE
-    offsets = block_start + tl.arange(0, BLOCK_SIZE)
-    mask = offsets < n_elements
+properties = driver.active.utils.get_device_properties(DEVICE.index)
+NUM_SM = properties["multiprocessor_count"]
+NUM_REGS = properties["max_num_regs"]
+SIZE_SMEM = properties["max_shared_mem"]
+WARP_SIZE = properties["warpSize"]
+target = triton.runtime.driver.active.get_current_target()
+kernels = {}
 
-    x = tl.load(x_ptr + offsets, mask=mask)
-    max_x = tl.libdevice.max(x, axis_ld)
-    x -= max_x
-    exp_x = tl.libdevice.exp(x / tau)
-    sum_exp_x = exp_x + axis_ld
-    output = exp_x / sum_exp_x
-    tl.store(output_ptr + offsets, output, mask=mask)
+def triton_softmax(x, tau):
+    n_rows, n_cols = x.shape
 
-def apply_activation(x: torch.Tensor, activation_fn: Any, *args, **kwargs):
-    """
-    Applies the specified activation function element-wise to the input tensor
-    """
-    output = torch.empty_like(x)
-    assert x.is_cuda and output.is_cuda
-    n_elements = output.numel()
-    grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
-    if "axis_ld" in kwargs:
-        axis_ld = kwargs.pop("axis_ld")
-        activation_fn[grid](
-            x, output, axis_ld, n_elements, BLOCK_SIZE=1024, *args, **kwargs
-        )
+    # The block size of each loop iteration is the smallest power of two greater than the number of columns in `x`
+    BLOCK_SIZE = triton.next_power_of_2(n_cols)
+
+    # Another trick we can use is to ask the compiler to use more threads per row by
+    # increasing the number of warps (`num_warps`) over which each row is distributed.
+    # You will see in the next tutorial how to auto-tune this value in a more natural
+    # way so you don't have to come up with manual heuristics yourself.
+    num_warps = 8
+
+    # Number of software pipelining stages.
+    num_stages = 4 if SIZE_SMEM > 200000 else 2
+
+    # Allocate output
+    y = torch.empty_like(x)
+
+    # pre-compile kernel to get register usage and compute thread occupancy.
+    kernel = softmax_kernel.warmup(y, x, x.stride(0), y.stride(0), n_rows, n_cols, BLOCK_SIZE=BLOCK_SIZE, num_stages=num_stages, tau=tau, num_warps=num_warps, grid=(1, ))
+    kernel._init_handles()
+    n_regs = kernel.n_regs
+    size_smem = kernel.metadata.shared
+    if is_hip():
+        # NUM_REGS represents the number of regular purpose registers. On CDNA architectures this is half of all registers available.
+        # However, this is not always the case. In most cases all registers can be used as regular purpose registers.
+        # ISA SECTION (3.6.4 for CDNA3)
+        # VGPRs are allocated out of two pools: regular VGPRs and accumulation VGPRs. Accumulation VGPRs are used
+        # with matrix VALU instructions, and can also be loaded directly from memory. A wave may have up to 512 total
+        # VGPRs, 256 of each type. When a wave has fewer than 512 total VGPRs, the number of each type is flexible - it is
+        # not required to be equal numbers of both types.
+        if is_cdna():
+            NUM_GPRS = NUM_REGS * 2
+
+        # MAX_NUM_THREADS represents maximum number of resident threads per multi-processor.
+        # When we divide this number with WARP_SIZE we get maximum number of waves that can
+        # execute on a CU (multi-processor)  in parallel.
+        MAX_NUM_THREADS = properties["max_threads_per_sm"]
+        max_num_waves = MAX_NUM_THREADS // WARP_SIZE
+        occupancy = min(NUM_GPRS // WARP_SIZE // n_regs, max_num_waves) // num_warps
     else:
-        activation_fn[grid](x, output, n_elements, BLOCK_SIZE=1024, *args, **kwargs)
-    return output
+        occupancy = NUM_REGS // (n_regs * WARP_SIZE * num_warps)
+    occupancy = min(occupancy, SIZE_SMEM // size_smem)
+    num_programs = NUM_SM * occupancy
 
-def triton_softmax(
-    x: torch.Tensor, axis_ld: Optional[Union[int, tuple[int, ...]]] = -1
-):
-    """
-    Applies the softmax activation function to the input tensor along the specified axis
-    """
-    if axis_ld is None:
-        axis_ld = 0
-    return apply_activation(x, functions.softmax_activation_kernel, axis_ld)
+    num_programs = min(num_programs, n_rows)
+
+    # Create a number of persistent programs.
+    kernel[(num_programs, 1, 1)](y, x, x.stride(0), y.stride(0), n_rows, n_cols, BLOCK_SIZE, num_stages)
+    return y
