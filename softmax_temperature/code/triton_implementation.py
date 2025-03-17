3fcb38e8fdc45bf9fc56eec44fed91e1a7b96320
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-17 12:06:44 -0600

    Update triton_implementation.py

--- softmax_temperature/code/triton_implementation.py
+++ softmax_temperature/code/triton_implementation.py
@@ -4,9 +4,19 @@ import triton
 import triton.language as tl
 from triton.runtime import driver
 
+DEVICE = triton.runtime.driver.active.get_active_torch_device()
+
+
+def is_hip():
+    return triton.runtime.driver.active.get_current_target().backend == "hip"
+
+
+def is_cdna():
+    return is_hip() and triton.runtime.driver.active.get_current_target().arch in ('gfx940', 'gfx941', 'gfx942',
+                                                                                   'gfx90a', 'gfx908')
+
 @triton.jit
-def softmax_kernel(output_ptr, input_ptr, input_row_stride, output_row_stride, n_rows, n_cols, BLOCK_SIZE: tl.constexpr,
-                   num_stages: tl.constexpr):
+def softmax_kernel(output_ptr, input_ptr, input_row_stride, output_row_stride, n_rows, n_cols, BLOCK_SIZE: tl.constexpr, num_stages: tl.constexpr, tau):
     # starting row of the program
     row_start = tl.program_id(0)
     row_step = tl.num_programs(0)
@@ -23,7 +33,7 @@ def softmax_kernel(output_ptr, input_ptr, input_row_stride, output_row_stride, n
         # Subtract maximum for numerical stability
         row_minus_max = row - tl.max(row, axis=0)
         # Note that exponentiation in Triton is fast but approximate (i.e., think __expf in CUDA)
-        numerator = tl.exp(row_minus_max)
+        numerator = tl.exp(row_minus_max) / tau
         denominator = tl.sum(numerator, axis=0)
         softmax_output = numerator / denominator
         # Write back output to DRAM
@@ -39,10 +49,11 @@ WARP_SIZE = properties["warpSize"]
 target = triton.runtime.driver.active.get_current_target()
 kernels = {}
 
-def softmax(x):
+
+def softmax(x, tau):
     n_rows, n_cols = x.shape
 
-    # The block size of each loop iteration is the smallest power of two greater than the number of columns in `x`
+    # The block size of each loop iteration is the minor power of two more significant than the number of columns in `x`
     BLOCK_SIZE = triton.next_power_of_2(n_cols)
 
     # Another trick we can use is to ask the compiler to use more threads per row by
@@ -57,25 +68,24 @@ def softmax(x):
     # Allocate output
     y = torch.empty_like(x)
 
-    # pre-compile kernel to get register usage and compute thread occupancy.
-    kernel = softmax_kernel.warmup(y, x, x.stride(0), y.stride(0), n_rows, n_cols, BLOCK_SIZE=BLOCK_SIZE,
-                                   num_stages=num_stages, num_warps=num_warps, grid=(1, ))
+    # Pre-compile kernel to get register usage and compute thread occupancy.
+    kernel = softmax_kernel.warmup(y, x, x.stride(0), y.stride(0), n_rows, n_cols, BLOCK_SIZE=BLOCK_SIZE, num_stages=num_stages, num_warps=num_warps, grid=(1, ), tau)
     kernel._init_handles()
     n_regs = kernel.n_regs
     size_smem = kernel.metadata.shared
     if is_hip():
-        # NUM_REGS represents the number of regular purpose registers. On CDNA architectures this is half of all registers available.
-        # However, this is not always the case. In most cases all registers can be used as regular purpose registers.
+        # NUM_REGS represents the number of regular purpose registers. On CDNA architectures, this is half of all registers available.
+        # However, this is not always the case. In most cases, all registers can be used for regular purposes.
         # ISA SECTION (3.6.4 for CDNA3)
         # VGPRs are allocated out of two pools: regular VGPRs and accumulation VGPRs. Accumulation VGPRs are used
-        # with matrix VALU instructions, and can also be loaded directly from memory. A wave may have up to 512 total
+        # with matrix VALU instructions and can be loaded directly from memory. A wave may have up to 512 total
         # VGPRs, 256 of each type. When a wave has fewer than 512 total VGPRs, the number of each type is flexible - it is
         # not required to be equal numbers of both types.
         if is_cdna():
             NUM_GPRS = NUM_REGS * 2
 
-        # MAX_NUM_THREADS represents maximum number of resident threads per multi-processor.
-        # When we divide this number with WARP_SIZE we get maximum number of waves that can
+        # MAX_NUM_THREADS represents the maximum number of resident threads per multi-processor.
+        # When we divide this number with WARP_SIZE, we get a maximum number of waves that can
         # execute on a CU (multi-processor)  in parallel.
         MAX_NUM_THREADS = properties["max_threads_per_sm"]
         max_num_waves = MAX_NUM_THREADS // WARP_SIZE
@@ -87,6 +97,6 @@ def softmax(x):
 
     num_programs = min(num_programs, n_rows)
 
-    # Create a number of persistent programs.
+    # Create several persistent programs.
     kernel[(num_programs, 1, 1)](y, x, x.stride(0), y.stride(0), n_rows, n_cols, BLOCK_SIZE, num_stages)
     return y
