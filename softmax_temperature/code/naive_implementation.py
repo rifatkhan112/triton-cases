78092e701721c02b286b98c4dae36be54a930a49
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-17 12:58:26 -0600

    Update naive_implementation.py

--- softmax_temperature/code/naive_implementation.py
+++ softmax_temperature/code/naive_implementation.py
@@ -6,11 +6,11 @@ def naive_softmax(x, tau):
     We subtract the maximum element to avoid overflows. Softmax is invariant to this shift.
     """
     # read  MN elements ; write M  elements
-    x_max = x.max(dim=1)[0] / tau
+    x_max = x.max(dim=1)[0]
     # read MN + M elements ; write MN elements
     z = x / tau - x_max[:, None]
     # read  MN elements ; write MN elements
-    numerator = torch.exp(z)
+    numerator = torch.exp(z / tau)
     # read  MN elements ; write M  elements
     denominator = numerator.sum(dim=1)
     # read MN + M elements ; write MN elements
