80339b52409f58cd8db1c5919125ebabb67bb5b7
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-17 13:03:59 -0600

    Update test.py

--- softmax_temperature/tests/test.py
+++ softmax_temperature/tests/test.py
@@ -1,10 +1,10 @@
 import torch
-
-from softmax_temperature.code.triton_implementation import softmax
+from softmax_temperature.code.naive_implementation import naive_softmax
+from softmax_temperature.code.triton_implementation import triton_softmax
 
 torch.manual_seed(0)
 x = torch.randn(1823, 781, device=DEVICE)
 tau = 100
-y_triton = softmax(x, tau)
-y_torch = torch.softmax(x, axis=1)
-assert torch.allclose(y_triton, y_torch), (y_triton, y_torch)
+y_triton = triton_softmax(x, tau)
+y_naive = naive_softmax(x, tau)
+assert torch.allclose(y_triton, y_naive), (y_triton, y_naive)
