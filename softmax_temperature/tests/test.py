04ae716bf1c0c39414eb67d6673e7d24d447761d
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-17 14:14:16 -0600

    Update test.py

--- softmax_temperature/tests/test.py
+++ softmax_temperature/tests/test.py
@@ -7,9 +7,28 @@ from triton.runtime import driver
 
 DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}')
 
-torch.manual_seed(0)
-x = torch.randn(1823, 781, device=DEVICE)
-tau = 100
-y_triton = triton_softmax(x)
-y_naive = naive_softmax(x, tau)
-assert torch.allclose(y_triton, y_naive), (y_triton, y_naive)
+def test_softmax_kernel(size: tuple, atol=1e-3, rtol=1e-3, device=DEVICE):
+    """
+    Here is where we test the wrapper function and kernel that we wrote 
+    above to ensure all our values are correct, using pytorch as the 
+    correct answer to compare against
+
+    we'll use an irregular number of rows & cols to verify that our padding mechanism works
+    """
+    # create input data
+    torch.manual_seed(0)
+    assert type(size) is tuple and len(size) == 2
+    x = torch.randn(size[0], size[1], device=DEVICE)
+    # run kernel & pytorch reference implementation
+    z_tri = triton_softmax(x)
+    z_ref = naive_softmax(x)
+        # notice our implementation doesn't give a choice for what axis to softmax along.
+        # this is a common theme of custom GPU kernels; because pytorch has to write code that
+        #  is more general, it is slower than it could be
+    # compare
+    torch.testing.assert_close(z_tri, z_ref, atol=atol, rtol=rtol)
+    print("PASSED")
+
+if __name__ == "__main__":
+    # always run unit-tests
+    test_softmax_kernel(size=(1823, 781))
