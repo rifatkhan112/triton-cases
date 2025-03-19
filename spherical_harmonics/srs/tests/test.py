cee8f4cb0ae23bccf28fd0f2630ca11ffe94312f
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-19 16:12:22 -0600

    Update test.py

--- spherical_harmonics/srs/tests/test.py
+++ spherical_harmonics/srs/tests/test.py
@@ -1,47 +1,13 @@
 import pytest
 import torch
 
-from equitriton import __HAS_XPU__, __HAS_CUDA__
-from equitriton.sph_harm.direct.utils import (
-    torch_spherical_harmonic,
-    triton_spherical_harmonic,
-)
+from spherical_harmonics.srs.code.naive_implementation import torch_spherical_harmonic
+from spherical_harmonics.srs.code.triton_implementation import triton_spherical_harmonic
 
 torch.manual_seed(316165)
 
-
 @pytest.mark.parametrize("order", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-@pytest.mark.parametrize(
-    "device",
-    [
-        pytest.param(
-            "xpu",
-            marks=pytest.mark.skipif(not __HAS_XPU__, reason="No XPUs available."),
-        ),
-        pytest.param(
-            "cuda",
-            marks=pytest.mark.skipif(
-                not __HAS_CUDA__, reason="No CUDA GPUs available."
-            ),
-        ),
-    ],
-)
 @pytest.mark.parametrize("tensor_shape", [(512, 3), (128, 16, 3), (256, 8, 8, 3)])
-@pytest.mark.parametrize(
-    "dtype",
-    [
-        pytest.param(
-            torch.float16,
-            marks=pytest.mark.xfail(raises=AssertionError, reason="low precision"),
-        ),
-        pytest.param(
-            torch.bfloat16,
-            marks=pytest.mark.xfail(raises=AssertionError, reason="low precision"),
-        ),
-        torch.float32,
-        torch.float64,
-    ],
-)
 
 def test_forward_equivalence(order, device, tensor_shape, dtype):
     """
@@ -55,44 +21,12 @@ def test_forward_equivalence(order, device, tensor_shape, dtype):
     assert torch.allclose(triton_out, torch_out, atol=1e-5, rtol=1e-3)
 
 @pytest.mark.parametrize("order", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
-@pytest.mark.parametrize(
-    "device",
-    [
-        pytest.param(
-            "xpu",
-            marks=pytest.mark.skipif(not __HAS_XPU__, reason="No XPUs available."),
-        ),
-        pytest.param(
-            "cuda",
-            marks=pytest.mark.skipif(
-                not __HAS_CUDA__, reason="No CUDA GPUs available."
-            ),
-        ),
-    ],
-)
-
 @pytest.mark.parametrize("tensor_shape", [(512, 3), (128, 16, 3), (256, 8, 8, 3)])
-@pytest.mark.parametrize(
-    "dtype",
-    [
-        pytest.param(
-            torch.float16,
-            marks=pytest.mark.xfail(raises=AssertionError, reason="low precision"),
-        ),
-        pytest.param(
-            torch.bfloat16,
-            marks=pytest.mark.xfail(raises=AssertionError, reason="low precision"),
-        ),
-        torch.float32,
-        torch.float64,
-    ],
-)
 
 def test_backward_equivalence(order, device, tensor_shape, dtype):
     """
-    Tests the numerical equivalence of the PyTorch versus
-    the Triton implementation of the backward pass. This is mostly to ensure that
-    writing outputs back out is being done correctly.
+    Tests the numerical equivalence of the PyTorch versus the Triton implementation of the backward pass. This is mainly to ensure that writing outputs back out is being done 
+    correctly.
     """
     coords = torch.rand(tensor_shape, device=device, dtype=dtype, requires_grad=True)
     # run with autograd first
