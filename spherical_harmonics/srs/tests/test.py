640be06deb1e890462017765727da34ecebd0d81
rifatkhan112 <rifat.khan.1559@gmail.com>
2025-03-19 16:22:32 -0600

    Update test.py

--- spherical_harmonics/srs/tests/test.py
+++ spherical_harmonics/srs/tests/test.py
@@ -4,18 +4,20 @@ import torch
 from spherical_harmonics.srs.code.naive_implementation import torch_spherical_harmonic
 from spherical_harmonics.srs.code.triton_implementation import triton_spherical_harmonic
 
+DEVICE = torch.device(f'cuda:{torch.cuda.current_device()}')
+
 torch.manual_seed(316165)
 
 @pytest.mark.parametrize("order", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 @pytest.mark.parametrize("tensor_shape", [(512, 3), (128, 16, 3), (256, 8, 8, 3)])
 
-def test_forward_equivalence(order, device, tensor_shape, dtype):
+def test_forward_equivalence(order, device=DEVICE, tensor_shape, dtype):
     """
     Tests the numerical equivalence of the PyTorch versus
     the Triton implementations. This is mostly to ensure that
     writing outputs back out is being done correctly.
     """
-    coords = torch.rand(tensor_shape, device=device, dtype=dtype)
+    coords = torch.rand(tensor_shape, device=DEVICE, dtype=dtype)
     triton_out = triton_spherical_harmonic(order, coords)
     torch_out = torch_spherical_harmonic(order, coords)
     assert torch.allclose(triton_out, torch_out, atol=1e-5, rtol=1e-3)
@@ -23,12 +25,12 @@ def test_forward_equivalence(order, device, tensor_shape, dtype):
 @pytest.mark.parametrize("order", [1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 @pytest.mark.parametrize("tensor_shape", [(512, 3), (128, 16, 3), (256, 8, 8, 3)])
 
-def test_backward_equivalence(order, device, tensor_shape, dtype):
+def test_backward_equivalence(order, device=DEVICE, tensor_shape, dtype):
     """
     Tests the numerical equivalence of the PyTorch versus the Triton implementation of the backward pass. This is mainly to ensure that writing outputs back out is being done 
     correctly.
     """
-    coords = torch.rand(tensor_shape, device=device, dtype=dtype, requires_grad=True)
+    coords = torch.rand(tensor_shape, device=DEVICE, dtype=dtype, requires_grad=True)
     # run with autograd first
     torch_out = torch_spherical_harmonic(order, coords)
     torch_out.backward(gradient=torch.ones_like(torch_out))
